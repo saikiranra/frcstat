ba75e2f65c41d364f39abad1ea7da85e4b9fb177
maherb <maherb@rpi.edu>
2019-06-03 21:23:10 -0400

    Added get methods to init for importing

--- frcstat/__init__.py
+++ frcstat/__init__.py
@@ -1,7 +1,7 @@
 from .TBA_Client import TBA_Client
-from .Season import Season , _Season_Set_TBA_Client
-from .Event import Event , _Event_Set_TBA_Client
-from .Team import Team , _Team_Set_TBA_Client
+from .Season import Season, getSeason, _Season_Set_TBA_Client
+from .Event import Event, getEvent, _Event_Set_TBA_Client
+from .Team import Team, getTeam, _Team_Set_TBA_Client
 
 def resetClient(client):
     _Singleton_TBA_Client = client
